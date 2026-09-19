defa34d8adad111b51ff042e60ff62c175892c27
awfajrii <awfajrii@gmail.com>
2026-09-19 19:01:43 +0700

    [Praktikum 6]: Model Eloquent, relasi antar tabel.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,4 +10,7 @@ class Category extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'description'];
+    public function products(){
+        return $this->hasMany(Product::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
@@ -28,5 +29,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function transactions()
+    {
+    return $this->hasMany(Transaction::class);
     }
 }
